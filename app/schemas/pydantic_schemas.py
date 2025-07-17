5e8960a26b7076535bc971f812b7325f227b5da0
Wang Xiuqiang <juice1750@outlook.com>
2025-07-17 10:01:22 +0800

    增加work2.md

--- app/schemas/pydantic_schemas.py
+++ app/schemas/pydantic_schemas.py
@@ -1,5 +1,5 @@
-from pydantic import BaseModel, ConfigDict
-from typing import List, Optional, Dict
+from pydantic import BaseModel, ConfigDict, Field
+from typing import List, Optional, Dict, Any
 from datetime import datetime
 import math
 
